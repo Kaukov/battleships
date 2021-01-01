efa9b14f847d9dec3500deceb79f4633584e28c7
Peter Kaukov <peter@kaukov.dev>
2021-01-01 21:17:16 +0200

    Add game data constants

--- battleships.rb
+++ battleships.rb
@@ -1,6 +1,25 @@
 require 'pry'
 
 class Battleships
+  # Battleship and empty space characters used in the input string
+  CHAR_SHIP = '*'
+  CHAR_EMPTYSPACE = '-'
+
+  # Define how many ships of type in total are allowed on the board
+  COUNT_BATTLESHIPS = 1
+  COUNT_CRUISERS = 2
+  COUNT_DESTROYERS = 3
+  COUNT_SUBMARINES = 4
+
+  # Define the size of all allowed ships
+  SIZE_BATTLESHIP = 4
+  SIZE_CRUISER = 3
+  SIZE_DESTROYER = 2
+  SIZE_SUBMARINE = 1
+
+  # The max ship size from all allowed ships
+  SIZE_MAX = SIZE_BATTLESHIP
+
   def initialize(input)
   end
 
